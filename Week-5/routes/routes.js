7606264b386a488b2ef640d50ee1ddbeaa4fe0e7
Mark Carlton <mcarlton33@gmail.com>
2023-01-25 10:08:01 +0300

    crud operations

--- Week-5/routes/routes.js
+++ Week-5/routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Team = require('../models/model');
 
 const router = express.Router();
 
@@ -6,21 +7,51 @@ module.exports = router;
 
 // CRUD routes
 // C - Create
-router.post('/teams', (req, res) => {
-    res.send('POST');
+router.post('/teams', async (req, res) => {
+    const data = new Team({
+        name: req.body.name,
+        stadium: req.body.stadium,
+    });
+    try {
+        // db.teams.insertOne
+        const savedData = await data.save();
+        res.status(201).json(savedData);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 // R - Read
-router.get('/teams', (req, res) => {
-    res.send('GET');
+router.get('/teams', async (req, res) => {
+    try {
+        const data = await Team.find();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 // U - Update
-router.patch('/teams/:id', (req, res) => {
-    res.send('UPDATE');
+router.patch('/teams/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+
+        const data = await Team.findByIdAndUpdate(id, updatedData, { new: true });
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 // D - Delete
-router.delete('/teams/:id', (req, res) => {
-    res.send('DELETE');
+router.delete('/teams/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const data = await Team.findByIdAndDelete(id);
+        res.status(204).json({ message: `The team named ${data.name} has been deleted` });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
