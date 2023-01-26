9abfcb59ae5ed3320560cbd5cc2294edc79e64e8
Mark Carlton <mcarlton33@gmail.com>
2023-01-26 10:23:27 +0300

    added a new contact route

--- Week-5-A/index.js
+++ Week-5-A/index.js
@@ -6,12 +6,14 @@ const app = express();
 const indexRoutes = require('./routes/index');
 const homeRoutes = require('./routes/home');
 const aboutRoutes = require('./routes/about');
+const contactRoutes = require('./routes/contact');
 
 const PORT = process.env.PORT || 3000
 
 app.use('/', indexRoutes);
 app.use('/home', homeRoutes);
 app.use('/about', aboutRoutes);
+app.use('/contactus', contactRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running');
---
+++ Week-5-A/routes/contact.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        console.log('Contact route');
+        res.status(200).json({ status: true });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+module.exports = router;
