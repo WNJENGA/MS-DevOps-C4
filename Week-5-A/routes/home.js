99eb9b4b0135f432b6f347cd6f86d7e57a8f7714
Mark Carlton <mcarlton33@gmail.com>
2023-01-26 09:35:48 +0300

    added more logging

--- Week-5-A/routes/home.js
+++ Week-5-A/routes/home.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
+        console.log('Home route');
         res.status(200).json({ status: true });
     } catch (error) {
         res.status(400).json({ message: error.message });
