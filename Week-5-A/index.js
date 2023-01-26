939caa4d3e61bbed6a49f9469a8fdd1db2275e8c
Mark Carlton <mcarlton33@gmail.com>
2023-01-26 09:17:38 +0300

    created a new server

---
+++ Week-5-A/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config()
+
+const app = express();
+
+const indexRoutes = require('./routes/index');
+const homeRoutes = require('./routes/home');
+
+const PORT = process.env.PORT || 3000
+
+app.use('/', indexRoutes);
+app.use('/home', homeRoutes);
+
+app.listen(PORT, () => {
+    console.log('Server is running');
+});
